2ae025b3a74aa3cf1960759e2a6c7c7ab951b38f
Kaue <kauelopesmoraes22@gmail.com>
2015-10-30 15:54:23 -0200

    +Comentario do Guilherme

--- controller/Core.js
+++ controller/Core.js
@@ -1,20 +1,40 @@
 //Modulos
-var com = require("serialport");
-var readline = require('readline');
-var fs = require('fs');
+var com = require('serialport'); 
+var express = require('express'); //FrameWork geral para http requests
+var path = require('path'); //Seguranca com acesso a arquivos
+var validacao = require('./Validacao');
 
 
 //Configuracoes
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
 var serialPort = new com.SerialPort("/dev/tty.usbmodem1421", {
     baudrate: 9600,
     parser: com.parsers.readline('\r\n')
-  });
+});
 
+var app = express();
+app.use(express.static(__dirname + "/../"));
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
+});
+
+
+
+
+//Eventos do Express
+app.get('/', function (req, res) {
+
+    fs.readFile(__dirname + "/../resources/dados/Log.txt", 'utf8', function (err,data) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(data);
+        res.send(data);
+
+    });
+});
 
 
 //Eventos da Porta Serial
@@ -25,14 +45,16 @@ serialPort.on('open',function() {
 
 serialPort.on('data', function(data) {
     serialPort.pause();    
-    verifica(data);
+    validacao.verifica(data, serialPort);
 });
 
 serialPort.on('close', function(data) {
-    rl.close();
+    validacao.dispose();
 });
 
 
+
+//Metodos Gerais
 
 /*
     Esse numero(argumento) do método verifica
@@ -47,59 +69,16 @@ serialPort.on('close', function(data) {
 
 */
 
-function verifica(numeroEnviadoPeloArduino){
-    rl.question(numeroEnviadoPeloArduino + " é valido? (Y/N)\n", function(answer) {
-        if(answer == "Y"){
-            //Sinal para abrir portao.            
-            serialPort.write("S"); //byte = 83
-            console.log("Portao foi aberto!\n");
-            addLog("ID:" + numeroEnviadoPeloArduino + " Acesso Permitido!");
 
-        }else{
-            console.log("Permissao Negada!\n");
-            addLog("ID:" + numeroEnviadoPeloArduino + " Acesso Negado!");
-        } 
-            
-
-            
-        serialPort.resume();
-        serialPort.flush();
-    });
-}
-
+//Funcoes Auxiliares
 
 //funcao apaga o conteudo do arquivo ou cria se não existe
 function iniciaNovoArquivoDeDados(){
-    fs.writeFile(__dirname + "/../resources/dados/Log.out", "", function(err) {
+    fs.writeFile(__dirname + "/../resources/dados/Log.txt", "", function(err) {
         if(err) {
             return console.log(err);
         }
         console.log("Arquivo alvo!");
     }); 
-}
-
-
-function addLog(conteudo){
-    var msg = conteudo + "  " + getTimeStamp() + "\n";
-    fs.appendFile(__dirname + "/../resources/dados/Log.out", msg, function (err) {
-        if(err){
-            return console.log(err);
-        }
-        console.log(conteudo);
-    });
-}
-
-function getTimeStamp() {
-    var now = new Date();
-    return ((now.getDate()) + '/' +
-            (now.getMonth() + 1) + '/' +
-             now.getFullYear() + " " +
-             now.getHours() + ':' +
-             ((now.getMinutes() < 10)
-                 ? ("0" + now.getMinutes())
-                 : (now.getMinutes())) + ':' +
-             ((now.getSeconds() < 10)
-                 ? ("0" + now.getSeconds())
-                 : (now.getSeconds())));
 }
 
---
+++ controller/Validacao.js
@@ -0,0 +1,67 @@
+//Modulos
+var readline = require('readline'); //Pega input do terminal
+var fs = require('fs'); //File System
+
+//configuracao 
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+//metodos para exportar
+module.exports = {
+    //Valida o numero de acordo com algum parametro
+    verifica: function (numeroEnviadoPeloArduino, serialPort){
+        rl.question(numeroEnviadoPeloArduino + " é valido? (Y/N)\n", function(answer) {
+            if(answer == "Y"){
+                //Sinal para abrir portao.            
+                serialPort.write("S"); //byte = 83
+                console.log("Portao foi aberto!\n");
+                addLog("ID:" + numeroEnviadoPeloArduino + " Acesso Permitido!");
+
+            }else{
+                console.log("Permissao Negada!\n");
+                addLog("ID:" + numeroEnviadoPeloArduino + " Acesso Negado!");
+            } 
+       
+            serialPort.resume();
+            serialPort.flush();
+        });
+    },
+
+    //Fecha o processo que faz a leitura das linhas
+    dispose: function (){
+        rl.close();
+    }   
+};
+
+
+
+
+//Funcoes privadas do modulo
+function addLog(conteudo){
+    var msg = conteudo + "  " + getTimeStamp() + "\n";
+    fs.appendFile(__dirname + "/../resources/dados/Log.txt", msg, function (err) {
+        if(err){
+            return console.log(err);
+        }
+        console.log(conteudo);
+    });
+}
+
+function getTimeStamp() {
+    var now = new Date();
+    return ((now.getDate()) + '/' +
+            (now.getMonth() + 1) + '/' +
+             now.getFullYear() + " " +
+             now.getHours() + ':' +
+             ((now.getMinutes() < 10)
+                 ? ("0" + now.getMinutes())
+                 : (now.getMinutes())) + ':' +
+             ((now.getSeconds() < 10)
+                 ? ("0" + now.getSeconds())
+                 : (now.getSeconds())));
+}
+
+
